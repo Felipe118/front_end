617a9781b758e378f8b16db3c4b261cda9980f81
@Felipe1181 <lipebezerraa@gmail.com>
2021-12-08 16:27:37 -0300

    Refazendo o rodape e ajustando a responsividade

---
+++ assets/js/dropdown.js
@@ -0,0 +1,14 @@
+function recolher(){
+   var contentMenu = document.getElementById('content')
+   
+   if(contentMenu.style.display ==  'block'){
+    contentMenu.style.display = 'none'
+   }else{
+    contentMenu.style.display = 'block' 
+   }
+
+
+    
+   //alert("clicado")
+
+}
